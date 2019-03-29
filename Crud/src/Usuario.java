a255a04baccddf2b356d7a44f8ca8593b561f22c
samaravile <samaravile@hotmail.com>
2019-03-29 19:38:43 -0300

    Adicionando Usuario.java.

---
+++ Crud/src/Usuario.java
@@ -0,0 +1,40 @@
+package modelo;
+
+public class Usuario {
+    Double id;
+    String nome;
+    String telefone;
+    String email; 
+    String senha;
+
+	public Double getId() {
+	    return id;
+	}
+	public void setId(Double id) {
+	    this.id = id;
+	}
+	public String getNome() { 
+	    return nome;
+	} 
+	public void setNome(String nome) { 
+	    this.nome = nome;
+	}
+	public String getTelefone() { 
+        return telefone;
+    } 
+    public void setTelefone(String telefone) { 
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
