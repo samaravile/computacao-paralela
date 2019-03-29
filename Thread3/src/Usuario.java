44ffb832af574e363e783f83cd5f35f6fd452fcd
samaravile <samaravile@hotmail.com>
2019-03-29 19:21:07 -0300

    Adicionando arquivo.

---
+++ Thread3/src/Usuario.java
@@ -0,0 +1,42 @@
+
+/* Trabalho feito por:
+ * 
+ * Isadora, Luis e Samara
+ * 
+ * */
+
+public class Usuario {
+	
+	private int codusuario;
+	private String nome;
+	private String tipo;
+	private String senha;
+	
+	public int getCodusuario() {
+		return codusuario;
+	}
+	public void setCodusuario(int codusuario) {
+		this.codusuario = codusuario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
